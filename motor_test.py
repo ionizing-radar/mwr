c164c4e01cc9742b53533160493a191357dda89b
kinetic energy released per unit mass <kerma@ionizing-radar.ca>
2018-01-01 21:31:47 +0000

    added basic motor testing

---
+++ motor_test.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+GPIO.setmode(GPIO.BOARD)
+ 
+Motor1A = 16
+Motor1B = 18
+Motor1E = 22
+
+Motor2A = 21
+Motor2B = 19
+Motor2E = 23
+
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor1E,GPIO.OUT)
+
+GPIO.setup(Motor2A,GPIO.OUT)
+GPIO.setup(Motor2B,GPIO.OUT)
+GPIO.setup(Motor2E,GPIO.OUT)
+ 
+print "Turning motor 1 on"
+GPIO.output(Motor1A,GPIO.HIGH)
+GPIO.output(Motor1B,GPIO.LOW)
+GPIO.output(Motor1E,GPIO.HIGH)
+ 
+sleep(5)
+ 
+print "Stopping motor"
+GPIO.output(Motor1E,GPIO.LOW)
+ 
+print "Turning motor 2 on"
+GPIO.output(Motor2A,GPIO.HIGH)
+GPIO.output(Motor2B,GPIO.LOW)
+GPIO.output(Motor2E,GPIO.HIGH)
+
+sleep(5)
+
+print "Stopping motor"
+GPIO.output(Motor2E,GPIO.LOW)
+
+GPIO.cleanup()
