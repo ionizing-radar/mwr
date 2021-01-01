64045dde19c54e1b586d041c7df6b9e7a5481a3d
kerma <kerma@ionizing-radar.ca>
2021-01-01 14:51:38 +0000

    echo server

--- rover_server.py
+++ rover_server.py
@@ -8,14 +8,17 @@ import socket
 HOST = '192.168.0.11'
 PORT = 23500
 
+# simple echo server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	print ('Listening on',HOST,':',PORT)
 	s.bind((HOST, PORT))
 	s.listen()
 	conn, addr = s.accept()
+	# make a connection
 	with conn:
 		print('Connect by ', addr)
 		while True:
+			# get data, echo it, loop
 			data = conn.recv(1024)
 			if not data:
 				break
